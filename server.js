294db1d59fe6ae6fbe2334c6e6a5c7111e6d1687
Mohamed Hassanin Omran <mohamed_hassanin_omran@yahoo.com>
2021-11-14 22:37:20 +0200

    Add: get and post routes

--- server.js
+++ server.js
@@ -2,34 +2,47 @@
 projectData = {};
 
 // Require Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
-const bodyParser= require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
 // Setup Server
 const port = 3000;
 
-
-
 // Express to run server and routes
 
+/**
+ * @ret the projectData object
+ */
+app.get("/api/data", (req, res) => {
+  res.status(200).send(projectData);
+});
 
+app.post("/api/data", (req, res) => {
+  const data = req.body;
+  if (data.temperature && data.date && data.userResponse) {
+    projectData = { ...data };
+    console.log(projectData);
+    res.status(200).send(projectData);
+  } else {
+    res.status(400).send("Invalid JSON object");
+  }
+});
 
 // Start up an instance of app
-app.listen(port, ()=>{
-    console.log(`running on localhost: ${port}`);
-})
-
+app.listen(port, () => {
+  console.log(`running on localhost: ${port}`);
+});
